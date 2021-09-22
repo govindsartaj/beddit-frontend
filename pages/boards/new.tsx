f01cf81fd467bb96dfb454bbced9dd46a14599bd
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-09-22 09:42:41 -0500

    build new board page

--- pages/boards/new.tsx
+++ pages/boards/new.tsx
@@ -4,6 +4,44 @@ const NewBoard: NextPage = () => {
   return (
     <div>
       <div className="font-light p-3 text-4xl">Create Board</div>
+      <div className="border-1 border-gray-100 h-auto m-2 p-4 bg-gray-50 rounded-lg shadow-md">
+        <div className="m-6">
+          <label>
+            Name your board. You can change this at any time!
+            <input
+              placeholder="Board Name"
+              className="w-full border p-1"
+            ></input>
+          </label>
+        </div>
+        <div className="m-6">
+          <label>
+            Pick a unique board ID for your board&apos;s URL. Choose wisely;
+            this cannot be changed later!
+            <div className="flex">
+              <div className="font-mono hidden sm:block p-1">
+                http://beddit.co/
+              </div>
+              <input
+                placeholder="unique-id"
+                className="w-full font-mono border p-1"
+              ></input>
+            </div>
+          </label>
+        </div>
+        <div className="m-6">
+          <label>
+            Describe your board. What will you be talking about here?
+            <input
+              placeholder="Description"
+              className="w-full border p-1"
+            ></input>
+          </label>
+        </div>
+      </div>
+      <div className="cursor-pointer hover:bg-red-100 w-1/2 sm:w-1/3 md:1/4 lg:w-1/5 flex justify-center float-right font-light mt-3 mx-3 p-2 bg-gray-100 border-black border rounded-lg text-2xl">
+        Create
+      </div>
     </div>
   );
 };
